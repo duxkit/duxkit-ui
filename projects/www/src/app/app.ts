e503e48801101cd886d2aa8e3f9fef8a09467a5e
Liam Langstaff <liamlangstaff@gmail.com>
2026-06-29 11:11:41 +0100

    Render global docs footer

--- projects/www/src/app/app.ts
+++ projects/www/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer.component';
 import { HeaderComponent } from './header.component';
 
 @Component({
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
